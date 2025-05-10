57c0dcbc064be7bcd5f87fc3ec7fe78631bfbad5
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-10 15:59:41 +0300

    fix name

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -1,5 +1,6 @@
-{inputs, ...}: {
-  anons-Mac-mini = (import ./donteatoreo/anon {inherit inputs;}).anons-Mac-mini;
-  faputa = (import ./bigshaq9999/nanachi {inherit inputs;}).faputa;
-  unsigned-int8 = (import ./ashuramaruzxc/unsigned-int8 {inherit inputs;}).unsigned-int8;
+{ inputs, ... }:
+{
+  anons-Mac-mini = (import ./donteatoreo/anon { inherit inputs; }).anons-Mac-mini;
+  faputa-Mac-mini = (import ./bigshaq9999/nanachi { inherit inputs; }).faputa-Mac-mini;
+  unsigned-int8 = (import ./ashuramaruzxc/unsigned-int8 { inherit inputs; }).unsigned-int8;
 }
