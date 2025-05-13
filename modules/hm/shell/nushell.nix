21bcb859b1deb7b1c993a83add9b3973aef7c1a5
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-13 12:09:09 +0300

    nushell refactor

--- modules/hm/shell/nushell.nix
+++ modules/hm/shell/nushell.nix
@@ -15,158 +15,161 @@ in
 {
   options.hm.nushell.enable = lib.mkEnableOption "Nushell";
 
-  config = lib.mkIf config.hm.nushell.enable {
-    programs.nushell =
-      {
-        enable = true;
-        package = inputs.nixpkgs-unstable.legacyPackages.${osConfig.nixpkgs.hostPlatform.system}.nushell;
-        shellAliases = {
-          # CD
-          cd = "z";
-          dc = "zi";
+  config = lib.mkIf (config.hm.nushell.enable && release > 25) {
+    programs.nushell = {
+      enable = true;
+      package =
+        inputs.nixpkgs-unstable-small.legacyPackages.${osConfig.nixpkgs.hostPlatform.system}.nushell;
+      plugins = builtins.attrValues {
+        inherit (pkgs.nushellPlugins)
+          formats
+          highlight
+          query
+          units
+          ;
+      };
+      shellAliases = {
+        # CD
+        cd = "z";
+        dc = "z";
 
-          # List Files
-          ls = "ls";
-          lt = "ls --all | sort-by size -r";
-          ll = "ls --all";
-          llf = ''ls --all | where type == "file"'';
-          ld = ''ls --all | find --regex "^\\."'';
+        # List Files
+        lt = "ls --all | sort-by size -r";
+        ll = "ls --all";
+        llf = ''ls --all | where type == "file"'';
+        ld = ''ls --all | find --regex "^\\."'';
 
-          # Time
-          now = ''date now | format date "%H:%M:%S"'';
-          nowtime = "date now";
-          nowdate = ''date now | format date "%d-%m-%Y"'';
-          nowunix = "date now | format date '%s'";
+        # Time
+        now = ''date now | format date "%H:%M:%S"'';
+        nowtime = "date now";
+        nowdate = ''date now | format date "%d-%m-%Y"'';
+        nowunix = "date now | format date '%s'";
 
-          # File Operations
-          mv = "mv -iv";
-          cp = "cp -iv";
-          rm = "rm -v";
-          mkdir = "mkdir -v";
-          untar = "tar -zxvf";
-          targz = "tar -cvzf";
+        # File Operations
+        mv = "mv -iv";
+        cp = "cp -iv";
+        rm = "rm -v";
+        mkdir = "mkdir -v";
+        untar = "tar -zxvf";
+        targz = "tar -cvzf";
 
-          # Video
-          m4a = "${yt-dlp-script} m4a";
-          m4a-cut = "${yt-dlp-script} m4a-cut";
-          mp3 = "${yt-dlp-script} mp3";
-          mp3-cut = "${yt-dlp-script} mp3-cut";
-          mp4 = "${yt-dlp-script} mp4";
-          mp4-cut = "${yt-dlp-script} mp4-cut";
+        # Video
+        m4a = "${yt-dlp-script} m4a";
+        m4a-cut = "${yt-dlp-script} m4a-cut";
+        mp3 = "${yt-dlp-script} mp3";
+        mp3-cut = "${yt-dlp-script} mp3-cut";
+        mp4 = "${yt-dlp-script} mp4";
+        mp4-cut = "${yt-dlp-script} mp4-cut";
 
-          # Misc
-          myip = "http get 'https://ipinfo.io/ip'";
-          mount = ''df -h | str replace "Mounted on" Mounted_On | detect columns'';
-          xdg-data-dirs = ''echo $env.XDG_DATA_DIRS | str replace -a ":" "\n" | lines | enumerate'';
-          path = ''echo $env.PATH'';
+        # Misc
+        myip = "http get 'https://ipinfo.io/ip'";
+        mount = ''df -h | str replace "Mounted on" Mounted_On | detect columns'';
+        xdg-data-dirs = ''echo $env.XDG_DATA_DIRS | str replace -a ":" "\n" | lines | enumerate'';
+        path = ''echo $env.PATH'';
 
-          nix-build-file = ''
-            def nix-build-file [
-                file: string, 
-                args: string = "{}"
-            ] {
-                nix-build -E $"with import (builtins.getFlake 'nixpkgs') {}; callPackage ./$file $args"
-            }
-          '';
-
-          rebuild = ''
-            def rebuild [] {
-                if (sys host | get name | str downcase | str contains "linux") {
-                    nixos-rebuild switch --use-remote-sudo --flake /etc/nixos/
-                } else {
-                    darwin-rebuild switch --flake /etc/nixos/
-                }
-            }
-          '';
-
-          update = ''
-            def update [] {
-                let nix_user = (whoami)
-                let raw_host = (hostname)
-                let is_darwin = (sys host | get name | str downcase | str contains "darwin")
-                let nix_host = if $is_darwin {
-                    # Only strip .local if present on macOS
-                    $raw_host | str replace -r '\.local$' \'\'
-                } else {
-                    $raw_host
-                }
-                let flake_path = "/etc/nixos"
-                let flake_eval_path = "/etc/nixos"
-                let flake_attr = if $is_darwin {
-                    "darwinConfigurations"
-                } else {
-                    "nixosConfigurations"
-                }
-                let nix_user_escaped = ($nix_user | str replace '"' '\\"')
-                let nix_host_escaped = ($nix_host | str replace '"' '\\"')
-                let github_username = (
-                    nix eval --raw --impure --expr (
-                        'let
-                          flake = builtins.getFlake "' + $flake_eval_path + '";
-                          host = flake.' + $flake_attr + '."' + $nix_host_escaped + '";
-                          user = "' + $nix_user_escaped + '";
-                        in
-                          host.config.home-manager.users.''${user}.programs.git.userName'
-                    ) | str downcase | str trim
-                )
-                let matching_inputs = (
-                    nix eval --json --impure --expr (
-                        '(builtins.attrNames (builtins.getFlake "' + $flake_eval_path + '").inputs)'
-                    )
-                    | from json
-                    | filter {|x| $x | str ends-with ("-" + $github_username)}
-                    | str join " "
-                )
-                nix flake update $matching_inputs --flake $flake_path
-            }
-          '';
-        };
-
-        configFile.text = ''
-          let $config = {
-            rm_always_trash: true
-            shell_integration: true
-            highlight_resolved_externals: true
-            use_kitty_protocol: true
-            completion_algorithm: "fuzzy"
+        nix-build-file = ''
+          def nix-build-file [
+              file: string, 
+              args: string = "{}"
+          ] {
+              nix-build -E $"with import (builtins.getFlake 'nixpkgs') {}; callPackage ./$file $args"
           }
         '';
 
-        extraConfig =
-          let
-            customCompletions = pkgs.fetchFromGitHub {
-              owner = "nushell";
-              repo = "nu_scripts";
-              rev = "618c0c035d15c3af7158ab122141c017acd454f5";
-              hash = "sha256-Tc1r1FrvLhfj6PzaLA1c6X3W7zL3UGhR4FSS3gRD+3g=";
-            };
-            completionTypes = [
+        rebuild = ''
+          def rebuild [] {
+              if (sys host | get name | str downcase | str contains "linux") {
+                  nixos-rebuild switch --use-remote-sudo --flake /etc/nixos/
+              } else {
+                  darwin-rebuild switch --flake /etc/nixos/
+              }
+          }
+        '';
+
+        update = ''
+          def update [] {
+              let nix_user = (whoami)
+              let raw_host = (hostname)
+              let is_darwin = (sys host | get name | str downcase | str contains "darwin")
+              let nix_host = if $is_darwin {
+                  # Only strip .local if present on macOS
+                  $raw_host | str replace -r '\.local$' \'\'
+              } else {
+                  $raw_host
+              }
+              let flake_path = "/etc/nixos"
+              let flake_eval_path = "/etc/nixos"
+              let flake_attr = if $is_darwin {
+                  "darwinConfigurations"
+              } else {
+                  "nixosConfigurations"
+              }
+              let nix_user_escaped = ($nix_user | str replace '"' '\\"')
+              let nix_host_escaped = ($nix_host | str replace '"' '\\"')
+              let github_username = (
+                  nix eval --raw --impure --expr (
+                      'let
+                        flake = builtins.getFlake "' + $flake_eval_path + '";
+                        host = flake.' + $flake_attr + '."' + $nix_host_escaped + '";
+                        user = "' + $nix_user_escaped + '";
+                      in
+                        host.config.home-manager.users.''${user}.programs.git.userName'
+                  ) | str downcase | str trim
+              )
+              let matching_inputs = (
+                  nix eval --json --impure --expr (
+                      '(builtins.attrNames (builtins.getFlake "' + $flake_eval_path + '").inputs)'
+                  )
+                  | from json
+                  | filter {|x| $x | str ends-with ("-" + $github_username)}
+                  | str join " "
+              )
+              nix flake update $matching_inputs --flake $flake_path
+          }
+        '';
+      };
+
+      configFile.text = ''
+        let $config = {
+          rm_always_trash: true
+          shell_integration: true
+          highlight_resolved_externals: true
+          use_kitty_protocol: true
+          completion_algorithm: "fuzzy"
+        }
+      '';
+
+      extraConfig =
+        let
+          customCompletions = pkgs.fetchFromGitHub {
+            owner = "nushell";
+            repo = "nu_scripts";
+            rev = "189c3d646e3ec6f76f74527e0aeb1586aad2127c";
+            hash = "sha256-ggvzb7OEAOajY7HN6UH9Q8DVu6G/PN5dRCJn66zua0s=";
+          };
+          completionTypes =
+            let
+              enableComp = n: lib.optional config.programs.${n}.enable [ n ];
+            in
+            [
               "bat"
               "curl"
-              "gh"
-              "git"
               "man"
               "nix"
               "rg"
-              "ssh"
-              "vscode"
-              "zellij"
-              "zoxide"
-            ];
-            sourceCommands = map (
-              t: "source ${customCompletions}/custom-completions/${t}/${t}-completions.nu"
-            ) completionTypes;
-          in
-          builtins.concatStringsSep "\n" sourceCommands;
-      }
-      // lib.optionalAttrs (release > 25) {
-        plugins = builtins.attrValues {
-          inherit (pkgs.nushellPlugins)
-            formats
-            # highlight
-            query
-            ;
-        };
-      };
+            ]
+            ++ enableComp "git"
+            ++ enableComp "gh"
+            ++ enableComp "ssh"
+            ++ enableComp "vscode"
+            ++ enableComp "zellij"
+            ++ enableComp "zoxide";
+
+          sourceCommands = map (
+            t: "source ${customCompletions}/custom-completions/${t}/${t}-completions.nu"
+          ) completionTypes;
+        in
+        builtins.concatStringsSep "\n" sourceCommands;
+    };
   };
 }
