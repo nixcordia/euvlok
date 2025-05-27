49f0b33d27d825ef403ad61855fdb8eff5c839ac
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-05-27 18:32:01 +0300

    chore(flake.*): use `git` to fetch `lix-module-source` instead of tarball

--- flake.nix
+++ flake.nix
@@ -91,7 +91,7 @@
     jj-vcs-source.inputs.nixpkgs.follows = "nixpkgs-unstable";
     jj-vcs-source.url = "github:jj-vcs/jj";
     lix-module-source.inputs.nixpkgs.follows = "nixpkgs-unstable";
-    lix-module-source.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+    lix-module-source.url = "git+https://git.lix.systems/lix-project/nixos-module?ref=2.93.0";
     nil-source.inputs.flake-utils.follows = "flake-utils";
     nil-source.inputs.nixpkgs.follows = "nixpkgs-unstable";
     nil-source.url = "github:oxalica/nil";
