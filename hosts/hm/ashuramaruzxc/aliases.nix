5349705ef9a4fc9eafc5de0dc3e81476637ef206
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-06-03 03:07:47 +0300

    chore: move video2gif script to hm/ashuramaruzxc

--- hosts/hm/ashuramaruzxc/aliases.nix
+++ hosts/hm/ashuramaruzxc/aliases.nix
@@ -1,12 +1,10 @@
 { pkgs, lib, ... }:
 let
-  utils = {
-    video2gif = lib.getExe (
-      pkgs.writeScriptBin "video2gif" (builtins.readFile ../../../pkgs/scripts/ashuramaruzxc/video2gif.sh)
-    );
+  aliases = {
+    video2gif = lib.getExe (pkgs.writeScriptBin "video2gif" (builtins.readFile ./scripts/video2gif.sh));
   };
 in
 {
-  programs.bash.shellAliases = utils;
-  programs.zsh.shellAliases = utils;
+  programs.bash.shellAliases = aliases;
+  programs.zsh.shellAliases = aliases;
 }
