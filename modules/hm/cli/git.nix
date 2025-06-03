50bf0fd10e8bf1a9d4bc177ede1f0c13f4283263
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-06-03 17:39:51 +0300

    feat(hm/cli/git): use `gitMinimal`, shadow `git grep` with `rg`

--- modules/hm/cli/git.nix
+++ modules/hm/cli/git.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   config,
   osConfig,
@@ -13,15 +14,21 @@
       programs = {
         gitui.enable = true;
         gh.enable = true;
-        git.enable = true;
-        git.ignores = [
-          "*.DS_Store"
-          "*.swp"
-          ".DS_Store"
-          "Thumbs.db"
-          "result"
-          "result*"
-        ];
+        git = {
+          enable = true;
+          package = pkgs.gitMinimal; # We don't use SVN; we also don't need GUI, manual, Python support, or PCRE2
+          ignores = [
+            "*.DS_Store"
+            "*.swp"
+            ".DS_Store"
+            "Thumbs.db"
+            "result"
+            "result*"
+          ];
+          aliases = {
+            grep = "!sh -c 'rg --vimgrep --color=always --line-number --no-heading --smart-case \"$@\"' _";
+          };
+        };
         vscode.profiles.default.userSettings = {
           git.enableCommitSigning = if (config.programs.git.signing.key != null) then true else false;
         };
