520646fd81ec5dd69245a05f068c82453642d5c7
DontEatOreo <57304299+DontEatOreo@users.noreply.github.com>
2025-06-03 18:49:53 +0300

    feat(hm/cli/git): qol for `programs.git.extraConfig`

--- modules/hm/cli/git.nix
+++ modules/hm/cli/git.nix
@@ -28,6 +28,32 @@
           aliases = {
             grep = "!sh -c 'rg --vimgrep --color=always --line-number --no-heading --smart-case \"$@\"' _";
           };
+          # Inspired by https://blog.gitbutler.com/how-git-core-devs-configure-git/
+          extraConfig = {
+            branch.sort = "-committerdate"; # Sort branches by most recent commit
+            column.ui = "auto"; # Display branches, tags, etc. in columns
+            commit.verbose = true; # Show diff of changes in commit message editor
+            core.fsmonitor = true; # Useful for big repos e.g nixpkgs; enables background file system monitoring
+            core.untrackedCache = true; # useful in conjunction with fsmonitor; speeds up finding untracked files
+            diff.algorithm = "histogram"; # Better quality diffs
+            diff.colorMoved = "plain"; # Highlight moved lines distinctly
+            diff.mnemonicPrefix = true; # Show i/, w/, c/ prefixes in diff headers
+            diff.renames = true;
+            fetch.prune = true; # Remove remote-tracking branches that no longer exist on the remote
+            fetch.pruneTags = true; # Remove local tags that no longer exist on the remote
+            help.autocorrect = "prompt"; # Suggest corrections for mistyped commands and prompt
+            init.defaultBranch = "trunk";
+            merge.conflictstyle = "zdiff3"; # Shows the common ancestor in merge conflicts for more context
+            pull.rebase = true; # Use rebase instead of merge for `git pull`
+            push.autoSetupRemote = true; # Automatically set upstream on first push to new branch
+            push.followTags = true;
+            rebase.autoSquash = true; # Automatically arrange fixup!/squash! commits during interactive rebase
+            rebase.autoStash = true; # Automatically stash changes before rebase and apply after
+            rebase.updateRefs = true; # Keep stacked branches up-to-date when rebasing
+            rerere.autoupdate = true; # Automatically stage files if conflicts were auto-resolved by rerere
+            rerere.enabled = true; # Reuse recorded resolutions of conflicted merges
+            tag.sort = "version:refname"; # Sort tags like versions (e.g., v1.10 after v1.2)
+          };
         };
         vscode.profiles.default.userSettings = {
           git.enableCommitSigning = if (config.programs.git.signing.key != null) then true else false;
