2b890ad34f827476740872034ec134ae402e7211
Tenjin <ashuramaru@tenjin-dk.com>
2025-09-01 17:59:05 +0200

    fix: sha256 in nvidia package

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -98,6 +98,7 @@
 
         NVD_BACKEND = "direct";
         LIBVA_DRIVER_NAME = "nvidia";
+        GSK_RENDERER = "ngl"; # temp solution until nvidia fixes wayland vulkan backend for gtk apps
       };
 
       hardware.nvidia = {
@@ -107,7 +108,7 @@
           version = "580.76.05";
           sha256_64bit = "sha256-IZvmNrYJMbAhsujB4O/4hzY8cx+KlAyqh7zAVNBdl/0=";
           sha256_aarch64 = lib.fakeSha256;
-          openSha256 = "sha256-IZvmNrYJMbAhsujB4O/4hzY8cx+KlAyqh7zAVNBdl/0=";
+          openSha256 = "sha256-xEPJ9nskN1kISnSbfBigVaO6Mw03wyHebqQOQmUg/eQ=";
           settingsSha256 = "sha256-ll7HD7dVPHKUyp5+zvLeNqAb6hCpxfwuSyi+SAXapoQ=";
           persistencedSha256 = lib.fakeSha256;
         };
