9245c51df9891213a7331cab251d477a177d7b07
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-10-07 01:56:19 +0300

    refactor(modules/hm/languages): use `lib.strings.toSentenceCase`, format

--- modules/hm/languages.nix
+++ modules/hm/languages.nix
@@ -3,11 +3,11 @@
   pkgsUnstable,
   lib,
   config,
+  osConfig,
   ...
 }:
 let
-  capitalize =
-    str: (lib.toUpper (lib.substring 0 1 str)) + (lib.substring 1 (lib.stringLength str) str);
+  inherit (osConfig.nixpkgs.hostPlatform) isDarwin;
 
   versionMappings = {
     java =
@@ -25,12 +25,13 @@ let
 
     dotnet =
       let
-        versionToPkgName = {
-          "8" = "sdk_8_0_3xx-bin";
-          "10" = "sdk_10_0-bin";
-        };
+        versions = [
+          "8"
+          "9"
+          "10"
+        ];
       in
-      lib.mapAttrs (version: pkgName: pkgsUnstable.dotnetCorePackages.${pkgName}) versionToPkgName;
+      lib.genAttrs versions (version: pkgsUnstable.dotnetCorePackages."sdk_${version}_0-bin");
   };
 
   getLatestVersion = mapping: lib.last (lib.sort lib.versionOlder (lib.attrNames mapping));
@@ -62,38 +63,18 @@ let
       };
     };
     csharp = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          omnisharp-roslyn
-          netcoredbg
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) omnisharp-roslyn netcoredbg; };
       versionMap = versionMappings.dotnet;
       defaultVersion = getLatestVersion versionMappings.dotnet;
     };
     dart = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          flutter
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) flutter; };
     };
     elixir = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          elixir
-          elixir-ls
-          hex
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) elixir elixir-ls hex; };
     };
     fsharp = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          dotnet-sdk
-          fsautocomplete
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) dotnet-sdk fsautocomplete; };
     };
     go = {
       packages = builtins.attrValues {
@@ -108,25 +89,21 @@ let
       };
     };
     haskell = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          ghc
-          cabal-install
-          stack
-          haskell-language-server
-          hlint
-          ormolu
-          ;
-      };
+      packages =
+        builtins.attrValues {
+          inherit (pkgsUnstable)
+            ghc
+            cabal-install
+            stack
+            haskell-language-server
+            hlint
+            ormolu
+            ;
+        }
+        ++ lib.optionals isDarwin builtins.attrValues { inherit (pkgsUnstable.haskellPackages) ghcup; };
     };
     java = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          jdt-language-server
-          gradle
-          maven
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) jdt-language-server gradle maven; };
       versionMap = versionMappings.java;
       defaultVersion = getLatestVersion versionMappings.java;
     };
@@ -148,21 +125,10 @@ let
       };
     };
     kotlin = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          kotlin
-          kotlin-language-server
-          gradle
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) kotlin kotlin-language-server gradle; };
     };
     lisp = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          sbcl
-          roswell
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) sbcl roswell; };
     };
     lua = {
       packages = builtins.attrValues {
@@ -175,49 +141,24 @@ let
       };
     };
     nim = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          nim
-          nimlsp
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) nim nimlsp; };
     };
     ocaml = {
       packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          ocaml
-          dune_3
-          opam
-          ;
-        inherit (pkgsUnstable.ocamlPackages)
-          ocaml-lsp
-          ocamlformat
-          ;
+        inherit (pkgsUnstable) ocaml dune_3 opam;
+        inherit (pkgsUnstable.ocamlPackages) ocaml-lsp ocamlformat;
       };
     };
     perl = {
       packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          perl
-          ;
-        inherit (pkgsUnstable.perlPackages)
-          PerlLanguageServer
-          PerlCritic
-          PerlTidy
-          ;
+        inherit (pkgsUnstable) perl;
+        inherit (pkgsUnstable.perlPackages) PerlLanguageServer PerlCritic PerlTidy;
       };
     };
     php = {
       packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          php
-          intelephense
-          ;
-        inherit (pkgsUnstable.phpPackages)
-          composer
-          psalm
-          phpstan
-          ;
+        inherit (pkgsUnstable) php intelephense;
+        inherit (pkgsUnstable.phpPackages) composer psalm phpstan;
       };
     };
     python =
@@ -234,17 +175,11 @@ let
         ]);
       in
       {
-        packages = builtins.attrValues {
-          python = python312;
-        };
+        packages = builtins.attrValues { python = python312; };
       };
     ruby = {
       packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          ruby_3_4
-          solargraph
-          rubocop
-          ;
+        inherit (pkgsUnstable) ruby_3_4 solargraph rubocop;
         inherit (pkgsUnstable.rubyPackages)
           rails
           rails-dom-testing
@@ -278,21 +213,10 @@ let
       };
     };
     swift = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          swift
-          swift-format
-          sourcekit-lsp
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) swift swift-format sourcekit-lsp; };
     };
     zig = {
-      packages = builtins.attrValues {
-        inherit (pkgsUnstable)
-          zig
-          zls
-          ;
-      };
+      packages = builtins.attrValues { inherit (pkgsUnstable) zig zls; };
     };
   };
 in
@@ -301,7 +225,7 @@ in
     name: def:
     lib.mkOption {
       default = { };
-      description = lib.options.mdDoc "Manages the development environment for the ${capitalize name} language.";
+      description = lib.options.mdDoc "Manages the development environment for the ${lib.strings.toSentenceCase name} language";
       type =
         if def ? versionMap then
           lib.types.submodule {
@@ -309,25 +233,25 @@ in
               enable = lib.mkOption {
                 type = lib.types.bool;
                 default = false;
-                description = lib.options.mdDoc "Enable the development environment and tools for ${capitalize name}.";
+                description = lib.options.mdDoc "Enable the development environment and tools for ${lib.strings.toSentenceCase name}";
               };
               version = lib.mkOption {
                 type = lib.types.enum (lib.attrNames def.versionMap);
                 default = def.defaultVersion;
                 description = lib.options.mdDoc ''
-                  Select the version of the ${capitalize name} SDK to install.
+                  Select the version of the ${lib.strings.toSentenceCase name} SDK to install
 
                   **Available versions:**
                   ${lib.concatStringsSep "\n" (map (v: "- `${v}`") (lib.attrNames def.versionMap))}
 
-                  The default is `${def.defaultVersion}`.
+                  The default is `${def.defaultVersion}`
                 '';
               };
               extraPackages = lib.mkOption {
                 type = lib.types.listOf lib.types.package;
                 default = [ ];
                 description = lib.options.mdDoc ''
-                  A list of extra packages to install alongside the standard ${capitalize name} toolchain.
+                  A list of extra packages to install alongside the standard ${lib.strings.toSentenceCase name} toolchain
                 '';
               };
             };
@@ -338,13 +262,13 @@ in
               enable = lib.mkOption {
                 type = lib.types.bool;
                 default = false;
-                description = lib.options.mdDoc "Enable the development environment and tools for ${capitalize name}.";
+                description = lib.options.mdDoc "Enable the development environment and tools for ${lib.strings.toSentenceCase name}";
               };
               extraPackages = lib.mkOption {
                 type = lib.types.listOf lib.types.package;
                 default = [ ];
                 description = lib.options.mdDoc ''
-                  A list of extra packages to install alongside the standard ${capitalize name} toolchain.
+                  A list of extra packages to install alongside the standard ${lib.strings.toSentenceCase name} toolchain.
                 '';
               };
             };
