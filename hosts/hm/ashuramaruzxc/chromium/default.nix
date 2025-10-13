5ca8197886bd87e16d9d1ebf9c780941dbd1c3fc
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-10-13 19:25:17 +0300

    chromium to revert

--- hosts/hm/ashuramaruzxc/chromium/default.nix
+++ hosts/hm/ashuramaruzxc/chromium/default.nix
@@ -4,8 +4,11 @@
   config,
   ...
 }:
+let
+  extensions = lib.flatten [ (pkgs.callPackage ./extensions.nix { inherit config; }) ];
+in
 {
-  programs.chromium.extensions = lib.flatten [
-    (pkgs.callPackage ./extensions.nix { inherit config; })
-  ];
+  hm.chromium.brave.extraExtensions = extensions;
+  hm.chromium.chromium.extraExtensions = extensions;
+  hm.chromium.vivaldi.extraExtensions = extensions;
 }
--- modules/hm/gui/chromium/default.nix
+++ modules/hm/gui/chromium/default.nix
@@ -5,68 +5,132 @@
   osConfig,
   ...
 }:
-{
-  options.hm.chromium = {
-    enable = lib.mkEnableOption "Chromium";
-    browser = lib.mkOption {
-      default = "ungoogled-chromium";
-      description = "Select the Chromium browser variant";
-      type = lib.types.enum [
-        "brave"
-        "chromium"
-        "google-chrome"
-        "microsoft-edge"
-        "ungoogled-chromium"
-        "vivaldi"
-      ];
-    };
+let
+  browserPackages = {
+    brave = pkgsUnstable.brave;
+    chromium = pkgsUnstable.chromium.override { enableWideVine = true; };
+    google-chrome = pkgsUnstable.google-chrome;
+    ungoogled-chromium = pkgsUnstable.ungoogled-chromium;
+    vivaldi = pkgsUnstable.vivaldi;
   };
 
-  config = lib.mkIf config.hm.chromium.enable {
-    assertions = [
-      {
-        assertion = osConfig.nixpkgs.hostPlatform.isLinux;
-        message = "Chromium is only available on Linux";
-      }
-    ];
-    programs.chromium = {
-      enable = true;
-      package =
-        let
-          browserPackages = {
-            chromium = pkgsUnstable.chromium.override { enableWideVine = true; };
-            inherit (pkgsUnstable)
-              brave
-              google-chrome
-              microsoft-edge
-              ungoogled-chromium
-              vivaldi
-              ;
-          };
-        in
-        browserPackages.${config.hm.chromium.browser};
-      dictionaries = builtins.attrValues {
-        inherit (pkgsUnstable.hunspellDictsChromium) en_US de_DE fr_FR;
-      };
-      extensions = (pkgsUnstable.callPackage ./extensions.nix { inherit config; });
-      commandLineArgs = [
-        # Debug
-        "--enable-logging=stderr"
-      ]
-      ++ lib.optionals (config.hm.chromium.browser == "chromium" || "google-chrome" || "microsoft-edge") [
-        # Enable mv2 while its still possible
-        "--disable-features=ExtensionManifestV2Unsupported,ExtensionManifestV2Disabled"
-      ]
-      ++ lib.optionals osConfig.nixpkgs.hostPlatform.isLinux [
-        "--ignore-gpu-blocklist"
-        "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder"
+  chromiumProgramUsers = [
+    "chromium"
+    "google-chrome"
+    "ungoogled-chromium"
+  ];
 
-        # Wayland
-        "--ozone-platform-hint=wayland"
-        "--enable-wayland-ime"
-        "--wayland-text-input-version=3"
-        "--enable-features=TouchpadOverscrollHistoryNavigation"
-      ];
+  getProgramName = name: if lib.elem name chromiumProgramUsers then "chromium" else name;
+
+  commonConfig = {
+    dictionaries = builtins.attrValues {
+      inherit (pkgsUnstable.hunspellDictsChromium) en_US de_DE fr_FR;
     };
+    extensions = (pkgsUnstable.callPackage ./extensions.nix { inherit config; });
+    commandLineArgs = [
+      # Debug
+      "--enable-logging=stderr"
+    ]
+    ++ lib.optionals osConfig.nixpkgs.hostPlatform.isLinux [
+      # Hardware Acceleration
+      "--ignore-gpu-blocklist"
+      "--enable-features=VaapiVideoDecoder,VaapiVideoEncoder"
+
+      # Wayland
+      "--ozone-platform-hint=wayland"
+      "--enable-wayland-ime"
+      "--wayland-text-input-version=3"
+      "--enable-features=TouchpadOverscrollHistoryNavigation"
+    ];
+  };
+
+in
+{
+  options.hm.chromium = lib.mkOption {
+    type = lib.types.attrsOf (
+      lib.types.submodule (
+        { name, config, ... }:
+        {
+          options = {
+            enable = lib.mkEnableOption "this Chromium-based browser instance";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = browserPackages.${name};
+              defaultText = lib.literalExpression "browserPackages.${name}";
+              description = "The package to use for this browser instance";
+            };
+
+            extraCommandLineArgs = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [ ];
+              description = "Extra command line arguments specific to this browser";
+              example = [ "--incognito" ];
+            };
+
+            extraExtensions = lib.mkOption {
+              type = lib.types.listOf lib.types.package;
+              default = [ ];
+              description = "Extra extensions specific to this browser instance";
+            };
+          };
+
+          config = lib.mkIf (config.enable && osConfig.nixpkgs.hostPlatform.isLinux) {
+            programs.${getProgramName name} = {
+              enable = true;
+              package = config.package;
+              dictionaries = commonConfig.dictionaries;
+              extensions = commonConfig.extensions ++ config.extraExtensions;
+              commandLineArgs =
+                commonConfig.commandLineArgs
+                ++ (lib.optionals (lib.elem name chromiumProgramUsers) [
+                  "--disable-features=ExtensionManifestV2Unsupported,ExtensionManifestV2Disabled"
+                ])
+                ++ config.extraCommandLineArgs;
+            };
+          };
+        }
+      )
+    );
+    default = { };
+    description = "Configuration for one or more Chromium-based browsers";
+    example = lib.literalExpression ''
+      {
+        ungoogled-chromium = {
+          enable = true;
+        };
+
+        brave = {
+          enable = true;
+          extraCommandLineArgs = [ "--incognito" ];
+        };
+
+        vivaldi = {
+          enable = true;
+          extraExtensions = [ pkgs.ublock-origin ];
+        };
+      }
+    '';
+  };
+
+  config = {
+    assertions = [
+      (
+        let
+          enabledBrowsers = lib.attrNames (
+            lib.filterAttrs (_: browserCfg: browserCfg.enable) config.hm.chromium
+          );
+          enabledChromiumProgramUsers = lib.intersectLists chromiumProgramUsers enabledBrowsers;
+        in
+        {
+          assertion = lib.length enabledChromiumProgramUsers <= 1;
+          message = ''
+            You have enabled multiple browsers that all use the 'programs.chromium' home-manager module:
+              ${lib.concatStringsSep ", " enabledChromiumProgramUsers}
+            Please enable only one of them at a time.
+          '';
+        }
+      )
+    ];
   };
 }
