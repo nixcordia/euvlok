7c05921dec69b4b88aac2b42c7da2f3ae06da9f4
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-10-25 17:37:06 +0300

    chore(yazi): bump

--- modules/hm/tui/yazi/default.nix
+++ modules/hm/tui/yazi/default.nix
@@ -15,23 +15,6 @@
   options.hm.yazi.enable = lib.mkEnableOption "Yazi";
 
   config = lib.mkIf config.hm.yazi.enable {
-    xdg.configFile = {
-      "yazi/plugins/smart-paste.yazi/main.lua".text = ''
-        --- @sync entry
-        return {
-          entry = function()
-            local h = cx.active.current.hovered
-            if h and h.cha.is_dir then
-              ya.manager_emit("enter", {})
-              ya.manager_emit("paste", {})
-              ya.manager_emit("leave", {})
-            else
-              ya.manager_emit("paste", {})
-            end
-          end,
-        }
-      '';
-    };
     home.packages = builtins.attrValues { inherit (pkgs) mediainfo exiftool clipboard-jh; };
     programs.yazi = {
       enable = true;
@@ -41,8 +24,8 @@
           pluginsRepo = pkgs.fetchFromGitHub {
             owner = "yazi-rs";
             repo = "plugins";
-            rev = "d1c8baab86100afb708694d22b13901b9f9baf00";
-            hash = "sha256-52Zn6OSSsuNNAeqqZidjOvfCSB7qPqUeizYq/gO+UbE=";
+            rev = "8f1d9711bcd0e48af1fcb4153c16d24da76e732d";
+            hash = "sha256-7vsqHvdNimH/YVWegfAo7DfJ+InDr3a1aNU0f+gjcdw=";
           };
         in
         {
@@ -51,29 +34,12 @@
           hide-preview = "${pluginsRepo}/hide-preview.yazi";
           max-preview = "${pluginsRepo}/max-preview.yazi";
           smart-enter = "${pluginsRepo}/smart-enter.yazi";
-          system-clipboard = pkgs.applyPatches {
-            src = pkgs.fetchFromGitHub {
-              owner = "orhnk";
-              repo = "system-clipboard.yazi";
-              rev = "4f6942dd5f0e143586ab347d82dfd6c1f7f9c894";
-              hash = "sha256-M7zKUlLcQA3ihpCAZyOkAy/SzLu31eqHGLkCSQPX1dY=";
-            };
-            patches = [
-              (pkgs.writeText "system-clipboard-fix.patch" ''
-                diff --git a/main.lua b/main.lua
-                index 0e77f6a7bd..666604668d 100644
-                --- a/main.lua
-                +++ b/main.lua
-                @@ -13,7 +13,7 @@
-                 
-                 return {
-                 	entry = function()
-                -		ya.manager_emit("escape", { visual = true })
-                +		ya.mgr_emit("escape", { visual = true })
-                 
-                 		local urls = selected_or_hovered()
-              '')
-            ];
+          smart-paste = "${pluginsRepo}/smart-paste.yazi";
+          system-clipboard = pkgs.fetchFromGitHub {
+            owner = "orhnk";
+            repo = "system-clipboard.yazi";
+            rev = "888026c6d5988bd9dc5be51f7f96787bb8cadc4b";
+            hash = "sha256-8YtYYxNDfQBTyMxn6Q7/BCiTiscpiZFXRuX0riMlRWQ=";
           };
         }
         // lib.optionalAttrs config.programs.git.enable { git = "${pluginsRepo}/git.yazi"; }
@@ -81,8 +47,8 @@
           starship = pkgs.fetchFromGitHub {
             owner = "Rolv-Apneseth";
             repo = "starship.yazi";
-            rev = "6a0f3f788971b155cbc7cec47f6f11aebbc148c9";
-            hash = "sha256-q1G0Y4JAuAv8+zckImzbRvozVn489qiYVGFQbdCxC98=";
+            rev = "a63550b2f91f0553cc545fd8081a03810bc41bc0";
+            hash = "sha256-PYeR6fiWDbUMpJbTFSkM57FzmCbsB4W4IXXe25wLncg=";
           };
         };
       initLua = builtins.concatStringsSep "\n" (
