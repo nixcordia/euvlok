6db2cfce635aec9b7f6ecfe30927b8f37e6504b4
FlameFlag <57304299+FlameFlag@users.noreply.github.com>
2025-12-04 19:42:46 +0200

    fix(yt-dlp): migrate over to `yt-dlp-ejs`

--- pkgs/yt-dlp.nix
+++ pkgs/yt-dlp.nix
@@ -1,49 +1,34 @@
 {
   lib,
   python3Packages,
+  atomicparsley,
+  deno,
   fetchFromGitHub,
   ffmpeg-headless,
-  rtmpdump,
-  atomicparsley,
-  pandoc,
   installShellFiles,
-  atomicparsleySupport ? true,
-  ffmpegSupport ? true,
-  rtmpSupport ? true,
-  withAlias ? false, # Provides bin/youtube-dl for backcompat
+  pandoc,
+  rtmpdump,
 }:
-let
-  optional-dependencies = {
-    default = with python3Packages; [
-      brotli
-      certifi
-      mutagen
-      pycryptodomex
-      requests
-      urllib3
-      websockets
-    ];
-    curl-cffi = [ python3Packages.curl-cffi ];
-    secretstorage = with python3Packages; [
-      cffi
-      secretstorage
-    ];
-  };
-in
+
 python3Packages.buildPythonApplication {
   pname = "yt-dlp";
-  # The websites yt-dlp deals with are a very moving target. That means that
-  # downloads break constantly. Because of that, updates should always be backported
-  # to the latest stable release.
-  version = "2025.11.12-unstable-2025-12-03";
+  version = "2025.11.12-unstable-2025-12-04";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "yt-dlp";
     repo = "yt-dlp";
-    rev = "f7acf3c1f42cc474927ecc452205d7877af36731";
-    hash = "sha256-pIc8Q546sAJ+k4CI5G0sodo7IsAOkFq/9a3WUq2lIkk=";
+    rev = "7ec6b9bc40ee8a21b11cce83a09a07a37014062e";
+    hash = "sha256-Em8FLcCizSfvucg+KPuJyhFZ5MJ8STTjSpqaTD5xeKI=";
   };
+
+  postPatch = ''
+    substituteInPlace yt_dlp/version.py \
+      --replace-fail "UPDATE_HINT = None" 'UPDATE_HINT = "Nixpkgs/NixOS likely already contain an updated version.\n       To get it run nix-channel --update or nix flake update in your config directory."'
+    substituteInPlace yt_dlp/networking/_curlcffi.py \
+      --replace-fail "if curl_cffi_version != (0, 5, 10) and not (0, 10) <= curl_cffi_version < (0, 14)" \
+      "if curl_cffi_version != (0, 5, 10) and not (0, 10) <= curl_cffi_version"
+  '';
 
   build-system = with python3Packages; [ hatchling ];
 
@@ -52,8 +37,21 @@ python3Packages.buildPythonApplication {
     pandoc
   ];
 
-  # expose optional-dependencies, but provide all features
-  dependencies = lib.flatten (lib.attrValues optional-dependencies);
+  dependencies = builtins.attrValues {
+    inherit (python3Packages)
+      brotli
+      certifi
+      cffi
+      curl-cffi
+      mutagen
+      pycryptodomex
+      requests
+      secretstorage
+      urllib3
+      websockets
+      yt-dlp-ejs
+      ;
+  };
 
   pythonRelaxDeps = [ "websockets" ];
 
@@ -72,24 +70,17 @@ python3Packages.buildPythonApplication {
     python devscripts/fish-completion.py completions/fish/yt-dlp.fish
   '';
 
-  # Ensure these utilities are available in $PATH:
-  # - ffmpeg: post-processing & transcoding support
-  # - rtmpdump: download files over RTMP
-  # - atomicparsley: embedding thumbnails
-  makeWrapperArgs =
-    let
-      packagesToBinPath =
-        [ ]
-        ++ lib.optional atomicparsleySupport atomicparsley
-        ++ lib.optional ffmpegSupport ffmpeg-headless
-        ++ lib.optional rtmpSupport rtmpdump;
-    in
-    lib.optionals (packagesToBinPath != [ ]) [
-      ''--prefix PATH : "${lib.makeBinPath packagesToBinPath}"''
-    ];
+  makeWrapperArgs = ''--prefix PATH : "${
+    lib.makeBinPath atomicparsley ffmpeg-headless deno rtmpdump
+  }"'';
 
-  # Requires network
-  doCheck = false;
+  checkPhase = ''
+    output=$($out/bin/yt-dlp -v 2>&1 || true)
+    if echo $output | grep -q "unsupported"; then
+      echo "ERROR: Found \"unsupported\" string in yt-dlp -v output."
+      exit 1
+    fi
+  '';
 
   postInstall = ''
     installManPage yt-dlp.1
@@ -99,13 +90,7 @@ python3Packages.buildPythonApplication {
       --fish completions/fish/yt-dlp.fish \
       --zsh completions/zsh/_yt-dlp
 
-    install -Dm644 Changelog.md README.md -t "$out/share/doc/yt-dlp"
-  ''
-  + lib.optionalString withAlias ''
+    install -Dm644 Changelog.md README.md -t "$out/share/doc/yt_dlp"
     ln -s "$out/bin/yt-dlp" "$out/bin/youtube-dl"
   '';
-
-  meta = {
-    mainProgram = "yt-dlp";
-  };
 }
