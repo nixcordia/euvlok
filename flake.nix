fb85adc029d0391c7b0237323d7bb72b9536ff5b
FlameFlag <github@flameflag.dev>
2026-01-25 02:09:24 +0200

    chore: `{lightly -> darkly}-source`

--- flake.nix
+++ flake.nix
@@ -54,6 +54,7 @@
     anime-cursors-source.inputs.flake-parts.follows = "flake-parts";
     anime-game-launcher-source.url = "github:ezKEa/aagl-gtk-on-nix";
     anime-game-launcher-source.inputs.flake-compat.follows = "";
+    darkly-source.url = "github:Bali10050/Darkly";
     # DO NOT override stylix utilities inputs
     # stylix-trivial.inputs.flake-parts.follows = "";
     # stylix-trivial.inputs.git-hooks.follows = "pre-commit-hooks";
