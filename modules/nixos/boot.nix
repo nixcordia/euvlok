cab6e9d9cb0ce7e79a99500d7c4c16fa9df797f4
4evy <git@evy.pink>
2026-07-30 18:35:00 +0300

    boot: harden systemd-boot defaults

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -1,6 +1,18 @@
-{ lib, config, ... }:
+{
+  config,
+  lib,
+  options,
+  ...
+}:
 let
   cfg = config.nixos.boot;
+  bootCountingSupported = lib.attrsets.hasAttrByPath [
+    "boot"
+    "loader"
+    "systemd-boot"
+    "bootCounting"
+    "enable"
+  ] options;
 in
 {
   _class = "nixos";
@@ -11,7 +23,20 @@ in
   };
 
   config = lib.modules.mkIf cfg.systemd-boot.enable {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot = {
+        enable = true;
+        # Keep rollback entries bounded without exposing an interactive
+        # kernel command editor.
+        configurationLimit = lib.modules.mkDefault 10;
+        editor = lib.modules.mkDefault false;
+      }
+      # bootCounting is newer than the release branch used by the Raspberry
+      # Pi host. Enable it when the evaluating Nixpkgs provides the option.
+      // lib.attrsets.optionalAttrs bootCountingSupported {
+        bootCounting.enable = lib.modules.mkDefault true;
+      };
+    };
   };
 }
