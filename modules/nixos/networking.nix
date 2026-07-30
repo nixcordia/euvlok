35b97c08e636d541f603610c02f996d46f82ce3d
4evy <git@evy.pink>
2026-07-30 18:35:00 +0300

    networking: make Ethernet bonding opt-in

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -10,28 +10,48 @@ in
     enable = lib.options.mkEnableOption "NetworkManager" // {
       default = true;
     };
-    ethernetBond.enable = lib.options.mkEnableOption "the shared bond0 Ethernet profile" // {
+
+    ethernet.enable = lib.options.mkEnableOption "the shared automatic Ethernet profile" // {
       default = true;
     };
+
+    ethernetBond.enable =
+      lib.options.mkEnableOption "enslaving the shared Ethernet profile to bond0"
+      // {
+        default = false;
+      };
   };
 
   config = lib.modules.mkMerge [
     (lib.modules.mkIf cfg.enable {
       networking.networkmanager.enable = true;
     })
-    (lib.modules.mkIf cfg.ethernetBond.enable {
-      networking.networkmanager.ensureProfiles.profiles = {
-        ethernet = {
-          connection = {
-            id = "ethernet";
-            type = "ethernet";
-            master = "bond0";
-            slave-type = "bond";
-          };
-          ipv4.method = "auto";
-          ipv6.method = "auto";
+
+    (lib.modules.mkIf (cfg.enable && cfg.ethernet.enable) {
+      networking.networkmanager.ensureProfiles.profiles.ethernet = {
+        connection = {
+          id = "ethernet";
+          type = "ethernet";
+        }
+        // lib.attrsets.optionalAttrs cfg.ethernetBond.enable {
+          master = "bond0";
+          slave-type = "bond";
         };
+        ipv4.method = "auto";
+        ipv6.method = "auto";
       };
     })
+
+    {
+      assertions = [
+        {
+          assertion = !cfg.ethernetBond.enable || (cfg.enable && cfg.ethernet.enable);
+          message = ''
+            nixos.networking.ethernetBond.enable requires both
+            nixos.networking.enable and nixos.networking.ethernet.enable.
+          '';
+        }
+      ];
+    }
   ];
 }
