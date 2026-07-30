f2ec94820b5e9b75ec788007a17bf71808ff0ac0
4evy <git@evy.pink>
2026-07-30 18:35:00 +0300

    kvmfr: honor shared memory configuration

--- hosts/linux/ashuramaruzxc/shared/system/kvmfr.nix
+++ hosts/linux/ashuramaruzxc/shared/system/kvmfr.nix
@@ -13,22 +13,22 @@ in
       enable = lib.options.mkEnableOption "shm";
 
       size = lib.options.mkOption {
-        type = lib.types.int;
-        default = "128";
+        type = lib.types.ints.positive;
+        default = 128;
         description = "Size of the shared memory device in megabytes.";
       };
       user = lib.options.mkOption {
-        type = lib.types.str;
+        type = lib.types.nonEmptyStr;
         default = "root";
         description = "Owner of the shared memory device.";
       };
       group = lib.options.mkOption {
-        type = lib.types.str;
+        type = lib.types.nonEmptyStr;
         default = "root";
         description = "Group of the shared memory device.";
       };
       mode = lib.options.mkOption {
-        type = lib.types.str;
+        type = lib.types.strMatching "[0-7]{4}";
         default = "0600";
         description = "Mode of the shared memory device.";
       };
@@ -38,8 +38,8 @@ in
   config = lib.modules.mkIf cfg.enable {
     boot.extraModulePackages = builtins.attrValues { inherit (config.boot.kernelPackages) kvmfr; };
     boot.initrd.kernelModules = [ "kvmfr" ];
-    boot.kernelParams = [ "kvmfr.static_size_mb=${toString cfg.shm.size}" ];
-    services.udev.extraRules = ''
+    boot.kernelParams = lib.lists.optional cfg.shm.enable "kvmfr.static_size_mb=${toString cfg.shm.size}";
+    services.udev.extraRules = lib.modules.mkIf cfg.shm.enable ''
       SUBSYSTEM=="kvmfr", OWNER="${cfg.shm.user}", GROUP="${cfg.shm.group}", MODE="${cfg.shm.mode}"
     '';
   };
