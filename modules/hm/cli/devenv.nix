37f15db7684c4b2fa14509914fe21b2d7637dd2f
4evy <git@evy.pink>
2026-08-13 13:11:00 +0300

    devenv: require Home Manager integration

--- modules/hm/cli/devenv.nix
+++ modules/hm/cli/devenv.nix
@@ -2,27 +2,18 @@
 {
   config,
   lib,
-  options,
   pkgs,
   ...
 }:
-let
-  hasDevenvModule = lib.attrsets.hasAttrByPath [ "programs" "devenv" ] options;
-in
 {
   options.euvlok.home.devenv.enable = lib.options.mkEnableOption "devenv" // {
     default = true;
   };
 
-  config = lib.modules.mkIf config.euvlok.home.devenv.enable (
-    lib.attrsets.optionalAttrs hasDevenvModule {
-      programs.devenv = {
-        enable = true;
-        package = euvlokInputs.devenv.packages.${pkgs.stdenv.hostPlatform.system}.default;
-      };
-    }
-    // lib.attrsets.optionalAttrs (!hasDevenvModule) {
-      home.packages = [ pkgs.devenv ];
-    }
-  );
+  config = lib.modules.mkIf config.euvlok.home.devenv.enable {
+    programs.devenv = {
+      enable = true;
+      package = euvlokInputs.devenv.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
+  };
 }
