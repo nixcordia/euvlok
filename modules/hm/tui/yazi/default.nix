9240a326fb15cd2db132b04e35422012228f6d8b
4evy <git@evy.pink>
2026-08-13 13:11:00 +0300

    yazi: use packaged plugins

--- modules/hm/tui/yazi/default.nix
+++ modules/hm/tui/yazi/default.nix
@@ -26,32 +26,21 @@ in
       enable = true;
       package = pkgs.unstable.yazi;
       shellWrapperName = "yy";
-      plugins =
-        let
-          pluginsRepo = pkgs.fetchFromGitHub {
-            owner = "yazi-rs";
-            repo = "plugins";
-            rev = "8f1d9711bcd0e48af1fcb4153c16d24da76e732d";
-            hash = "sha256-7vsqHvdNimH/YVWegfAo7DfJ+InDr3a1aNU0f+gjcdw=";
-          };
-        in
-        {
-          diff = "${pluginsRepo}/diff.yazi";
-          full-border = "${pluginsRepo}/full-border.yazi";
-          smart-enter = "${pluginsRepo}/smart-enter.yazi";
-          smart-paste = "${pluginsRepo}/smart-paste.yazi";
-          system-clipboard = ./system-clipboard.yazi;
-          types = "${pluginsRepo}/types.yazi";
-        }
-        // lib.attrsets.optionalAttrs config.programs.git.enable { git = "${pluginsRepo}/git.yazi"; }
-        // lib.attrsets.optionalAttrs config.programs.starship.enable {
-          starship = pkgs.fetchFromGitHub {
-            owner = "Rolv-Apneseth";
-            repo = "starship.yazi";
-            rev = "a63550b2f91f0553cc545fd8081a03810bc41bc0";
-            hash = "sha256-PYeR6fiWDbUMpJbTFSkM57FzmCbsB4W4IXXe25wLncg=";
-          };
-        };
+      plugins = {
+        inherit (pkgs.unstable.yaziPlugins)
+          diff
+          full-border
+          smart-enter
+          smart-paste
+          ;
+        system-clipboard = ./system-clipboard.yazi;
+      }
+      // lib.attrsets.optionalAttrs config.programs.git.enable {
+        inherit (pkgs.unstable.yaziPlugins) git;
+      }
+      // lib.attrsets.optionalAttrs config.programs.starship.enable {
+        inherit (pkgs.unstable.yaziPlugins) starship;
+      };
       initLua = ''
         local function setup_plugin(name, opts)
           local ok, plugin = pcall(require, name)
