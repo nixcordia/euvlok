a03ebd9b01d42211f27ddaf0aa102f4971279a9f
ashuramaruzxc <ashuramaru@tenjin-dk.com>
2026-09-02 12:23:55 +0200

    chore(nixos): remove gconf2 from udev

--- modules/nixos/services.nix
+++ modules/nixos/services.nix
@@ -16,10 +16,7 @@
       gnome.gnome-keyring.enable = true;
       gnome.gnome-settings-daemon.enable = true;
       dbus.packages = builtins.attrValues { inherit (pkgs.unstable) gcr; };
-      udev.packages = builtins.attrValues {
-        inherit (pkgs.unstable) gnome-settings-daemon;
-        inherit (pkgs.unstable.gnome2) GConf;
-      };
+      udev.packages = builtins.attrValues { inherit (pkgs.unstable) gnome-settings-daemon; };
     };
   };
 }
