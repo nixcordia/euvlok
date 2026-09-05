4c164fcb0f8a2c510eb8f0b4b912b86f484e3fee
bigshaq9999 <97749920+bigshaq9999@users.noreply.github.com>
2026-09-05 15:34:31 +0700

    add sops secret for freshrss

--- hosts/linux/ashuramaruzxc/unsigned-int64/services/freshrss.nix
+++ hosts/linux/ashuramaruzxc/unsigned-int64/services/freshrss.nix
@@ -1,11 +1,17 @@
-_: {
+{ config, ... }:
+{
+  sops.secrets.freshrss = {
+    mode = ""; # Not sure what to set
+    owner = config.users.users.freshrss.name;
+    group = config.users.users.freshrss.group;
+  };
   services.freshrss = {
     enable = true;
     virtualHost = "freshrss.tenjin-dk.com";
     baseUrl = "https://freshrss.tenjin-dk.com";
     webserver = "nginx";
     defaultUser = "admin";
-    passwordFile = "/etc/secrets/freshrss"; # should this be in sops?
+    passwordFile = config.sops.secrets.freshrss.path;
   };
 
   services.nginx.virtualHosts."freshrss.tenjin-dk.com" = {
